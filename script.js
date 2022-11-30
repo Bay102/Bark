6c3590643c5a552f277b5fc173ca7496bd44ee58
Zak Bay <zacharybay@live.com>
2022-11-29 22:34:42 -0800

    SRP fetch

--- script.js
+++ script.js
@@ -1,83 +1,80 @@
-// Fetch data from an API //
-
-const createCards = (dog) => {
-  dogDiv = document.createElement("div");
-  dogDiv.innerHTML = ` 
-  <div class="cardsClass">   
-  <h2> ${dog.name} </h2> 
-  <i class="addToFav fa-sharp fa-solid fa-heart-circle-plus"></i>
-  <i class="removeFav fa-solid fa-heart-crack"></i> 
-  <img src=${dog.photoUrl} >
-  <div class="info-wrapper">   
-  <div> <strong> Breed : </strong> ${dog.breed} </div>  
-  <div> <strong> Age: </strong> ${dog.age}</div> 
-  </div>          
-  </div>
-  `;
-  
-  document.getElementById("adoptionCards").appendChild(dogDiv);
-}
-
-  
-
-  const dogsFetch = () => {
-   fetch("https://freerandomapi.cyclic.app/api/v1/dogs?limit=20")
-  .then((data) => {
-    return data.json();
-  })
-  .then((completedData) => {
-    completedData.data.map((dog) => {
-      createCards(dog); 
-
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  }); 
-  }
-  dogsFetch();
-  
   const favesDiv = document.getElementById("faves");   
   const adoptCardsDiv = document.getElementById("adoptionCards"); 
 
+const getData = async () => {
+  return await fetch("https://freerandomapi.cyclic.app/api/v1/dogs?limit=30")
+  .then((res) => res.json())
+  .then(data => data.data); 
+}
+
+const createSingleCard = (dog) => { 
+    dogDiv = document.createElement("div");
+      dogDiv.innerHTML = ` 
+      <div class="cardsClass">   
+      <h2> ${dog.name} </h2> 
+      <i class="addToFav fa-sharp fa-solid fa-heart-circle-plus" onClick="add"></i>
+      <i class="removeFav fa-solid fa-heart-crack"></i> 
+      <img src=${dog.photoUrl} >
+      <div class="info-wrapper">   
+      <div> <strong> Breed : </strong> ${dog.breed} </div>  
+      <div> <strong> Age: </strong> ${dog.age}</div> 
+      </div>          
+      </div>
+      `;
+      adoptCardsDiv.appendChild(dogDiv);
+  }
+
+const createAllCards = (data) => {
+  data.map((data) => {
+    createSingleCard(data)
+  })
+}
+
+// const addEventListeners = (className) => {
+//  const querySel = document.querySelector(className)
+//  className.querySel.addEventListener('click', (e) => {
+
+//  })
+// }
+
+  
+
+const createInitialAppState = async () => {
+  data = await getData();
+  createAllCards(data);
+  // add()
+  // addEventListeners('addToFav')
+}
+createInitialAppState();
+
+ const favButton = document.getElementsByClassName("addToFav"); 
+ const removeFav = document.getElementsByClassName("removeFav"); 
+
+  // function add() {
+  // querySelector('addToFav').addEventListener('click', (e) => {
+  //  let parent = e.target.parentElement;
+  //  favesDiv.append(parent)
+  // })
+
+
+
+// removeFav.style.display = "none";
+
+//       favButton.addEventListener("click", (e) => {
+//         let parent = e.target.parentElement; // the parent element of the event target ?
+//         dogDiv.append(parent);
+//         removeFav.style.display = "block";
+//         favButton.style.display = "none";
+//       });
+//       removeFav.addEventListener("click", (e) => {
+//         let parent = e.target.parentElement;
+//         adoptCardsDiv.append(parent);
+//         favButton.style.display = "block";
+//         removeFav.style.display = "none";
+//       });
  
-  let favButton = document.querySelectorAll(".addToFav"); console.log(favButton); 
-  let removeFav = document.querySelectorAll(".removeFav"); console.log(removeFav);
- 
 
 
-
-// const favoriteDog = () => { 
-//    .addEventListener("click", (e) => {
-// //       let parent = e.target.parentElement; // the parent element of the event target ?
-// //       dogDiv.append(parent);
-// //       removeFav.style.display = "block";
-// //       favButton.style.display = "none";
-// //     });
-// // };
-
-// //   favoriteDog();
-
-
-  //  removeFav.style.display = "none";
-
-      // favButton.addEventListener("click", (e) => {
-      //   let parent = e.target.parentElement; // the parent element of the event target ?
-      //   dogDiv.append(parent);
-      //   removeFav.style.display = "block";
-      //   favButton.style.display = "none";
-      // });
-      // removeFav.addEventListener("click", (e) => {
-      //   let parent = e.target.parentElement;
-      //   adoptCardsDiv.append(parent);
-      //   favButton.style.display = "block";
-      //   removeFav.style.display = "none";
-      // });
- 
-
-// Build function to add selected items to a favorite list
-
-// Build a function to remove items and place back into collection
 
 
   
