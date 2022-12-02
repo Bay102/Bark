f9ec6f549cebfbfe2dd640c95b8db7d263edb272
Zak Bay <zacharybay@live.com>
2022-12-01 22:22:50 -0800

    Progress! New Favorites Section Logic Complete

--- script.js
+++ script.js
@@ -4,28 +4,30 @@ const getData = async () => {
     .then((data) => data.data);
 };
 
+const favesDiv = document.getElementById("faves");
+const adoptCardsDiv = document.getElementById("adoptionCards");
+
 const createSingleCard = (dog) => {
   dogDiv = document.createElement("div");
   dogDiv.innerHTML = ` 
       <div class="cardsClass">   
       <h2> ${dog.name} </h2> 
-      <i class="addToFav fa-sharp fa-solid fa-heart-circle-plus"></i>
-      <img src=${dog.photoUrl} >
       <div class="info-wrapper">   
       <div> <strong> Breed : </strong> ${dog.breed} </div>  
       <div> <strong> Age: </strong> ${dog.age}</div> 
-      </div>          
+      </div>  
+      <img src=${dog.photoUrl} >
+      <i class="addToFav fa-sharp fa-solid fa-heart-circle-plus"></i>     
       </div>
       `;
-      adoptCardsDiv.appendChild(dogDiv);  
+  adoptCardsDiv.appendChild(dogDiv);
 };
-
 
 const createAllCards = (data) => {
   data.map((data) => {
     createSingleCard(data);
   });
-  addEventListenersToFav(getFavButtons); ///// added event listeners here .
+  addEventListenersToFav(getFavButtons);
 };
 
 const launchCards = async () => {
@@ -34,16 +36,9 @@ const launchCards = async () => {
 };
 launchCards();
 
-
-const favesDiv = document.getElementById("faves");
-const adoptCardsDiv = document.getElementById("adoptionCards");
-const getFavButtons =  document.getElementsByClassName("addToFav"); 
-const getRemoveFavButtons = document.getElementsByClassName("removeFav"); 
-
-
-
-const addEventListenersToFav = (favBtns) => {
-  for (const button of favBtns) {
+const getFavButtons = document.getElementsByClassName("addToFav");
+const addEventListenersToFav = (faveBtns) => {
+  for (const button of faveBtns) {
     button.addEventListener("click", (e) => {
       let parent = e.target.parentElement.parentElement;
       button.classList.add("fa-heart-crack");
@@ -51,13 +46,13 @@ const addEventListenersToFav = (favBtns) => {
       button.classList.remove("addToFav");
       button.classList.add("removeFav");
       favesDiv.append(parent);
-      addEventListenersToUnFav(getRemoveFavButtons); 
+      addEventListenersToUnFav(getRemoveFavButtons);
     });
   }
 };
-
-const addEventListenersToUnFav = (removeBtn) => {
-  for (const button of removeBtn) {
+const getRemoveFavButtons = document.getElementsByClassName("removeFav");
+const addEventListenersToUnFav = (removeBtns) => {
+  for (const button of removeBtns) {
     button.addEventListener("click", (e) => {
       let parent = e.target.parentElement.parentElement;
       adoptCardsDiv.append(parent);
@@ -69,3 +64,38 @@ const addEventListenersToUnFav = (removeBtn) => {
   }
 };
 
+
+
+
+
+
+
+
+const modalOpen = '[data-open]';
+const modalClose = '[data-close]';
+const isVisible = 'is-visible';
+
+// this will store any element that has "[data-open]" in a node list 
+const openModal = document.querySelectorAll(modalOpen);
+
+// this will store any element that has "[data-close]" in a node list 
+const closeModal = document.querySelectorAll(modalClose);
+
+//
+// const closeService = document.querySelectorAll(serviceClose);
+
+
+// for of loop to iterate through node list of queried elements above 
+//  and adding eventlistener to each one 
+for (const elm of openModal) { // open Modal buttons
+   elm.addEventListener('click', function() {
+      const modalId = this.dataset.open;  // "this" is referring to the parent Element
+      document.getElementById(modalId).classList.add(isVisible)
+   })
+}
+
+for (const elm of closeModal) { // close modal buttons 
+   elm.addEventListener('click', function() {
+      this.parentElement.parentElement.parentElement.classList.remove(isVisible)
+   })
+}
